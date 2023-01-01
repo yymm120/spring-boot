2a1f1ce110274807c3dcf99dd6386c312bc35f42
lxh <1208401731@qq.com>
2023-01-01 18:27:32 +0800

    aaaaaaaaaaa

---
+++ crazy-springboot/chapter1/src/main/java/org/springframework/boot/Main.java
@@ -0,0 +1,7 @@
+package org.springframework.boot;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ xinhaoluo/src/main/java/com/frontstore/FrontStoreApplication.java
@@ -0,0 +1,9 @@
+package com.frontstore;
+
+
+public class FrontStoreApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
